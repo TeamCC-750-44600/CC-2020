39407e6216b30264542d00ad8c0e0790903ae51b
MckinleyX <32435060+MckinleyX@users.noreply.github.com>
2019-11-27 20:49:57 -0600

    Cleaning up code, adding comments

--- fft_440.ino
+++ fft_440.ino
@@ -1,6 +1,6 @@
 #define MIC_PIN A0
-#include <fix_fft.h>
-#define SEARCH_FREQ 6//440 hertz, found experimentally
+#include <fix_fft.h> // github.com/kosme/fix_fft
+#define SEARCH_FREQ 6// 440 hertz, found experimentally
 //Increase about 100 hertz per output
 
 char im[128], data[128];
@@ -14,15 +14,15 @@ void setup()
 void loop()
 {
 	//int min = 1024, max = 0;
-	for(int ii = 0; ii < 128; ii++)//Take 128 samples
+	for(int ii = 0; ii < 128; ii++)		// Take 128 samples
 	{
-		val = analogRead(MIC_PIN);
-		data[ii] = val/4 - 120;//Populate array
-		im[ii] = 0;
+		val = analogRead(MIC_PIN);	// Read in value
+		data[ii] = val/4 - 120;		// Populate array
+		im[ii] = 0;			//
 	}
-	fix_fft(data, im, 7, 0);//Dark magic happens here
-	int dat = sqrt(data[SEARCH_FREQ]*data[SEARCH_FREQ] + im[SEARCH_FREQ] * im[SEARCH_FREQ]);
-	if(dat > 3)
-		Serial.print("Sound detected\t");
+	fix_fft(data, im, 7, 0);		// Dark magic happens here
+	int dat = sqrt(data[SEARCH_FREQ]*data[SEARCH_FREQ] + im[SEARCH_FREQ] * im[SEARCH_FREQ]); //Clean up values? Not really sure, but this seems important
+	if(dat > 3)				// If it's above a certain tolerance
+		Serial.print("Sound detected\t");//Not on newline so you can see how fast it's going
 }
 
--- fft_test.ino
+++ fft_test.ino
@@ -1,5 +1,5 @@
 #define MIC_PIN A0
-#include <fix_fft.h>
+#include <fix_fft.h> // github.com/kosme/fix_fft
 
 char im[128], data[128];
 int val;//Counter
@@ -12,19 +12,19 @@ void setup()
 void loop()
 {
 	//int min = 1024, max = 0;
-	for(int ii = 0; ii < 128; ii++)//Take 128 samples
+	for(int ii = 0; ii < 128; ii++)		//Take 128 samples
 	{
-		val = analogRead(MIC_PIN);
-		data[ii] = val/4 - 120;//Populate array
-		im[ii] = 0;
+		val = analogRead(MIC_PIN);	//Read in value
+		data[ii] = val/4 - 120;		//Populate array
+		im[ii] = 0;			//???
 	}
-	fix_fft(data, im, 7, 0);//Dark magic happens here
-	for(char ii = 1; ii < 64; ii++)
+	fix_fft(data, im, 7, 0);		//Dark magic happens here
+	for(char ii = 1; ii < 64; ii++)		//Print out all values
 	{
-		int dat = sqrt(data[ii]*data[ii] + im[ii] * im[ii]);
+		int dat = sqrt(data[ii]*data[ii] + im[ii] * im[ii]);//Clean up values? Not really sure; but this seems important
 		Serial.print(dat);
 		Serial.print(" ");
-	}
+	}					//Note: Printing all values such that you can compare a value with the one above it
 	Serial.print("\n");
 }
 
